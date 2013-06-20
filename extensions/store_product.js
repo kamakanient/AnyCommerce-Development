06d011fefea247a06c5ea5e23120ffca177a09ab
Taras I <rr-m@rr-m.org>
2013-06-20 21:58:52 +0300

    QuickView - 'Currently out of stock' notice

--- extensions/store_product.js
+++ extensions/store_product.js
@@ -422,7 +422,7 @@ $display.appendTo($tag);
 					$tag.show().removeClass('displayNone').removeAttr('disabled');
 					}
 				else	{
-					$tag.hide().addClass('displayNone').before("<span class='notAvailableForPurchase'>This item is not available for purchase<\/span>"); //hide button, item is not purchaseable.
+					$tag.hide().addClass('displayNone').before("<span class='notAvailableForPurchase'>Currently out of stock.<\/span>"); //hide button, item is not purchaseable.
 					}
 
 //				app.u.dump(" -> ID at end: "+$tag.attr('id'));
